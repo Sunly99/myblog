c6e38f588ca51035641fc93091c307f7727307f8
sly <942133823@qq.com>
2020-12-10 16:46:01 +0800

    添加注释

--- src/main/java/myblog/MyblogApplication.java
+++ src/main/java/myblog/MyblogApplication.java
@@ -3,6 +3,10 @@ package myblog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 孙柳烨
+ * @date 2020/12/10
+ */
 @SpringBootApplication
 public class MyblogApplication {
 
