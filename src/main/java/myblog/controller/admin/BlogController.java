c20f82ee8dff4cfd1f3ef30e8d7d6d7f8dca8232
sly <942133823@qq.com>
2020-12-11 16:02:08 +0800

    控制器添加

--- src/main/java/myblog/controller/admin/BlogController.java
+++ src/main/java/myblog/controller/admin/BlogController.java
@@ -1,4 +1,15 @@
 package myblog.controller.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author 孙柳烨
+ * @date 2020/12/11
+ */
+@Controller
+@RequestMapping("/admin")
 public class BlogController {
+
 }
+
